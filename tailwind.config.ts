06dc62779a04c0b2e02b15701b4dcc2a8f23a9f3
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-23 21:44:38 +0800

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "wh-10": "#F4F4F4",
+        "wh-50": "#FBFBFB",
+        "wh-100": "#C9C9C9",
+        "wh-300": "#939393",
+        "wh-500": "#595959",
+        "wh-900": "#0F0F0F",
+        "accent-red": "#EA9648",
+        "accent-orange": "#F6CF68",
+        "accent-green": "#C2E9B4",
       },
+      backgroundImage: (theme) => ({
+        "gradient-gradual":
+          "linear-gradient(180deg, rgba(116, 116, 116, 0) 66.15%, #000000 100%)",
+      }),
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
 export default config;
